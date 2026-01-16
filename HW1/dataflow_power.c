ee906b50d2b3ee6fe5b0bd078ff3e7dd9a46beeb
averageandyyy <averageandyyy@gmail.com>
2026-01-16 14:03:04 -0500

    change answer

--- HW1/dataflow_power.c
+++ HW1/dataflow_power.c
@@ -1,18 +1,23 @@
 int power(int x)
 {
     // Input guaranteed to be non-negative
+    int output = 0;
+
     if (x == 0)
     {
-        return 0;
+        return output;
     }
 
-    int output = 1;
+    // First pass
+    output += 1;
+    x -= 1;
 
     while (x > 0)
     {
         output += output;
-        x = x - 1;
+        x -= 1;
     }
 
+    // The algorithm computes 2^(x - 1) except when x = 0, returning 0 then
     return output;
 }
